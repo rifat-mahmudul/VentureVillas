168718c23fcb977e6b8e08816f70eb67d3e719b2
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-10-30 18:43:28 +0600

    Project Set Up Done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-xl text-center">Mahhmudul Hasan Rifat</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAlCVNK-DU_K4e7_IItDZeh2enSW35AHR4",
+    authDomain: "venturevillas-8a8fc.firebaseapp.com",
+    projectId: "venturevillas-8a8fc",
+    storageBucket: "venturevillas-8a8fc.appspot.com",
+    messagingSenderId: "1062986838225",
+    appId: "1:1062986838225:web:866dadebbb894d9e1215f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
